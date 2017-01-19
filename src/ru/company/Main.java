28ca651b3ee8e160470cdb3f7fe6cb255031852d
kemelian <kemelian_1960@mail.ru>
2017-01-19 20:37:22 +0200

    Initial commit

---
+++ src/ru/company/Main.java
@@ -0,0 +1,11 @@
+package ru.company;
+
+/**
+ * Created by константин on 19.01.2017.
+ */
+public class Main {
+    public static void main(String ... args) {
+        System.out.println("RRRRRRRRRRR");
+
+    }
+}
